1f032cc1ae16459d5c17133b7e4544758556d8c7
Valentina Butrico <vale.butrico@gmail.com>
2023-07-24 12:38:39 -0300

    primer intento de un menú deplegable responsive con js

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let toggleBtn = document.querySelector(".toggle-btn");
+let toggleBtnIcon = document.querySelector(".toggle-btn i");
+let desplegable = document.querySelector(".desplegable");
+
+toggleBtn.onclick = function () {
+  desplegable.classList.toggle("open");
+  const isOpen = desplegable.classList.contains("open");
+
+  toggleBtnIcon.classList = isOpen
+  ? "fa-solid fa-xmark"
+  : "fa-solid fa-bars"
+}
